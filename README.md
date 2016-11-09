3f04a54dfa01249fc7a6d73015945995f2598806
cbosselut <bossone0013@gmail.com>
2016-11-09 16:38:22 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+#img2pixel
+##img to what ?
+One method used to draw with CSS is to use :after pseudo-element and box-shadow argument to create pixels. It's a powerful solution but somehow complex to do by hand. This command line java tool use opencv to create the complete css for you based on an image and optionally on a pixel size (4px by default). 
+##Usage
+```bash
+java -jar img2pixel.jar avatar_80x80.png 16
+```
+Output:
+```html
+<style>
+#pixel{
+	width:80px;
+	height:80px;
+	}
+#pixel:after{
+	content:'';
+	display:block;
+	width:16px;
+	height:16px;
+	background:#000000;
+	box-shadow:0px 0px #000000,
+	0px 16px #000000,
+	0px 32px #000000,
+	0px 48px #000000,
+	0px 64px #000000,
+	16px 0px #000000,
+	16px 16px #000000,
+	16px 32px #010101,
+	16px 48px #030303,
+	16px 64px #000000,
+	32px 0px #000000,
+	32px 16px #000000,
+	32px 32px #DBDBDB,
+	32px 48px #848484,
+	32px 64px #010101,
+	48px 0px #A8A8A8,
+	48px 16px #979797,
+	48px 32px #E9E9E9,
+	48px 48px #313131,
+	48px 64px #1D1D1D,
+	64px 0px #000000,
+	64px 16px #000000,
+	64px 32px #C3C3C3,
+	64px 48px #575757,
+	64px 64px #0C0C0C;
+}
+</style><div id="pixel"></div>
+```
---
+++ src/main/java/Main.java
@@ -0,0 +1,66 @@
+import org.bytedeco.javacpp.opencv_core;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import static org.bytedeco.javacpp.opencv_imgcodecs.*;
+
+/**
+ * Copyright (C) 2015 Cyril Bosselut <bossone0013@gmail.com>
+ * <p>
+ * This file is part of NeoJava examples for UDOO
+ * <p>
+ * NeoJava examples for UDOO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This libraries are distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+public class Main {
+    public static void main(String[] args){
+        if(args.length > 0 && args[0] != null){
+            opencv_core.IplImage img = cvLoadImage(args[0]);
+            if (img != null) {
+                int width = img.width();
+                int height = img.height();
+                int pixelSize = 4;
+                if (args.length > 1) {
+                    pixelSize = Integer.parseInt(args[1]);
+                }
+                List<String> pixels = new ArrayList<String>();
+                String firstPixel = "#000000";
+                for (int i = 0; i < width; i += pixelSize) {
+                    for (int j = 0; j < height; j += pixelSize) {
+                        opencv_core.CvScalar scalar = opencv_core.cvGet2D(img, j, i);
+                        if(i == 0 && j == 0){
+                            firstPixel = String.format(Locale.getDefault(), "#%02X%02X%02X", (int) scalar.val(2), (int) scalar.val(1), (int) scalar.val(0));
+                        }
+                        String pixel = String.format("%dpx %dpx #%02X%02X%02X",
+                                i, j, (int) scalar.val(2), (int) scalar.val(1), (int) scalar.val(0));
+                        pixels.add(pixel);
+                    }
+                }
+                img.release();
+                @SuppressWarnings("Since15") String css = String.format(Locale.getDefault(),
+                        "<style>\n#pixel{\n\twidth:%dpx;\n\theight:%dpx;\n\t}\n#pixel:after{\n\tcontent:'';\n\tdisplay:block;\n\twidth:%dpx;\n\theight:%dpx;\n\tbackground:%s;\n\tbox-shadow:%s;\n}\n</style><div id=\"pixel\"></div>",
+                        width, height, pixelSize, pixelSize, firstPixel, String.join(",\n\t", pixels));
+                System.out.println(css);
+            }
+            else{
+                System.err.println("Can't open file " + args[0]);
+            }
+        }
+        else{
+            System.err.println("img2pixel needs at least one argument which is the source image path");
+        }
+    }
+}
