a1f5a697649e3957c23329f08196a32c5ff707e3
cbosselut <bossone0013@gmail.com>
2016-11-09 17:11:14 +0100

    update license

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,21 +7,21 @@ import java.util.Locale;
 import static org.bytedeco.javacpp.opencv_imgcodecs.*;
 
 /**
- * Copyright (C) 2015 Cyril Bosselut <bossone0013@gmail.com>
+ * Copyright (C) 2016 Cyril Bosselut <bossone0013@gmail.com>
  * <p>
- * This file is part of NeoJava examples for UDOO
+ * This file is part of img2pixel
  * <p>
- * NeoJava examples for UDOO is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * img2pixel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 1 of the License, or
  * (at your option) any later version.
  * <p>
- * This libraries are distributed in the hope that it will be useful,
+ * This application is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * <p>
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
