857e029f55a032201a2eb4201506b5d9a548ed8a
cbosselut <bossone0013@gmail.com>
2016-11-09 17:11:41 +0100

    update license

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ import static org.bytedeco.javacpp.opencv_imgcodecs.*;
  * <p>
  * img2pixel is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 1 of the License, or
+ * the Free Software Foundation, either version 2.1 of the License, or
  * (at your option) any later version.
  * <p>
  * This application is distributed in the hope that it will be useful,
